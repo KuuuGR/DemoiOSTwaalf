5350a1d552f20ce7428c11be09f7423c236ffb8a
Grzegorz Kulesza <gkulesza@9bits.pl>
2018-06-21 11:03:34 +0200

    create project

---
+++ DemoiOSTwaalf/DemoiOSTwaalf/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoiOSTwaalf
+//
+//  Created by Grzesiek Kulesza on 21.06.2018.
+//  Copyright © 2018 Grzesiek Kulesza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
